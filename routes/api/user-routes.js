7c44edb36a4f450b7faf82e4b4ef004a057c4bcf
Leo Ayala <leo_a1016@yahoo.com>
2020-08-27 18:49:13 -0400

    added a way to read encrypted passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll() ({
+    User.findAll({
       attributes: { exclude: ['password' ]}
     })
       .then(dbUserData => res.json(dbUserData))
@@ -50,6 +50,28 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+        // res.json({ user: dbUserData });
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect password!' });
+          return;
+        }
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });    
+      });  
+    });
   
 
 // PUT /api/users/1
