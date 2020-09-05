a834bbf6ecdc0b2595bff86b900cbd62e7829255
Leo Ayala <leo_a1016@yahoo.com>
2020-09-05 14:45:27 -0400

    added Post Model

--- models/Post.js
+++ models/Post.js
@@ -2,9 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {
-
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
@@ -41,5 +39,6 @@ Post.init(
       modelName: 'post'
     }
   );
+
 
   module.exports = Post;
